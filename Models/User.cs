cd9b4e4e7b4539a284ea8ade19b19f67fe6ae2e4
abhik1109 <73633400+abhik1109@users.noreply.github.com>
2023-04-29 17:13:04 +0530

    Added required attribute to user model

--- Models/User.cs
+++ Models/User.cs
@@ -1,8 +1,12 @@
-﻿namespace CoreAuthAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreAuthAPI.Models
 {
     public class User
     {
+        [Required]
         public string UserName { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
